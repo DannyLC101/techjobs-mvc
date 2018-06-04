13ff56ad335f5f9733bd1762aeeab8521e2da59c
Dhawani <dhawani.patel197@gmail.com>
2018-06-04 01:06:27 -0500

    techJob-MVC assignment complete

--- src/main/java/org/launchcode/controllers/HomeController.java
+++ src/main/java/org/launchcode/controllers/HomeController.java
@@ -3,6 +3,7 @@ package org.launchcode.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.servlet.mvc.Controller;
 
 import java.util.HashMap;
 
